8345f36cf0b925d9ceb5d4b9f987840c3ed8caff
nehorosiyparen <sir.kuzin09@yandex.ru>
2025-11-26 16:29:30 +0400

    update: main models files for modules added

---
+++ backend/app/modules/auth/models/user_model.py
@@ -0,0 +1,4 @@
+"""
+Docstring for backend.app.modules.auth.models.user.model
+Тут будет описываться модель ПОЛЬЗОВАТЕЛЯ с помощью base.sqlalchemy-repo.py
+"""
---
+++ backend/app/modules/notify/models/notification_model.py
@@ -0,0 +1,4 @@
+"""
+Docstring for backend.app.modules.notify.models.notification.model
+Здесь будет описываться модель УВЕДОМЛЕНИЯ с помощью base.sqlalchemy-repo.py
+"""
