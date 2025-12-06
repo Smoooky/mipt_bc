602dc5c5c8280181766c8c3ad1aef3ccb7366db6
nehorosiyparen <sir.kuzin09@yandex.ru>
2025-12-06 18:50:16 +0400

    fix: engine+session rescheduled

--- backend/app/core/app.py
+++ backend/app/core/app.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 from app.modules import register_modules
 from app.core.middlewares import register_middlewares
 from app.core.config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 class App: 
     def __init__(self):
         self.fastapi_app = FastAPI(title=settings.APP_NAME)
+        self.enigine = create_async_engine(
+            settings.DATABASE_URL,
+            echo=False,
+        )
+        self.session = async_sessionmaker(
+            self.engine,
+            autoflush=False,
+            expire_on_commit=False,
+        )
     
     def setup(self):
         """Настройка приложения: роуты, middlewares и т.д."""
